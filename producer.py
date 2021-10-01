1aaac81db7f87f0ccfc67b8a1f89aae1293cc699
Priyanshu Shukla <priyanshu.shukla@iedaas.com>
2021-10-01 17:46:00 +0530

    producer updated

---
+++ producer.py
@@ -0,0 +1,22 @@
+from confluent_kafka import Producer
+import socket
+
+conf = {'bootstrap.servers': "localhost:9092",
+        'client.id': socket.gethostname()}
+
+producer = Producer(conf)
+
+def acknw(error,message):
+    if error is not None:
+        print("Failed to send messages : {}:{} ".format(message,error))
+    else:
+        print("Message sent succesfully")
+       
+with open("data.json",'r') as data:
+    content = data.read()
+    producer.produce("student_data" ,key = "sample1",value = content , callback = acknw(None,"hi"))
+    producer.flush()
+
+
+
+
