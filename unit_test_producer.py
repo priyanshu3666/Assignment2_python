3280360ce6e5db58f097f9bd236fb8778b108007
Priyanshu Shukla <priyanshu.shukla@iedaas.com>
2021-10-05 15:52:40 +0530

    logging added

--- unit_test_producer.py
+++ unit_test_producer.py
@@ -1,13 +1,19 @@
 import unittest
 from producer.producer import acknw
+import logging
 
+logging.basicConfig(filename="logfile.log",format='%(asctime)s %(message)s',filemode='w')
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
 class  TestProducer(unittest.TestCase):
     
     def test_acknw(self):
         self.assertEqual(acknw(None,"hi"),None)
+        logging.info("test_acknw successfully tested")
         
     def test_type_compatibilty_acknw(self):
         self.assertRaises(TypeError,acknw(None,123465))
+        logging.info("test_type_compatibilty_acknw  successfully tested")
     
 if __name__ == '__main__':
     unittest.main()
