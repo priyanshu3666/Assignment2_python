1a0668bdae98a0449963abce2ab134aa5f6c9390
Priyanshu Shukla <priyanshu.shukla@iedaas.com>
2021-10-11 16:30:37 +0530

    db loader removed

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,96 +1,72 @@
-import logging
+import logging,csv,os,json
 from confluent_kafka import Consumer
 from confluent_kafka import KafkaError,KafkaException
-import sys
-from sqlalchemy import MetaData,Table,VARCHAR,Column,Integer,create_engine, engine
-import json
-from sqlalchemy.sql.schema import Constraint, ForeignKey, ForeignKeyConstraint
-
-from sqlalchemy.sql.sqltypes import BIGINT
-
-meta =MetaData()
-engine = create_engine("mysql+mysqlconnector://shukla:a123456@0.0.0.0:3306/sample",echo = True)
-connection = engine.connect()
-Table(
-    'student', meta,
-    Column('stu_id', Integer, primary_key=True),
-    Column('firstName', VARCHAR(50)),
-    Column('lastName', VARCHAR(50)),
-    Column('gender', VARCHAR(11)),
-    Column('age', Integer),
-    Column('streetAddress', VARCHAR(30)),
-    Column('city', VARCHAR(20)),
-    Column('state', VARCHAR(20)),
-    Column('postalCode',Integer()),
-    
-)
-Table(
-    'student_contact', meta,
-    Column('cont_id', Integer, primary_key=True, autoincrement=True),
-    Column('stu_id', Integer,ForeignKey("student.stu_id")),
-    Column('type', VARCHAR(10)),
-    Column('number', BIGINT())
-)
-
-meta.create_all(engine)
+import random
+import string
 conf = {'bootstrap.servers': "localhost:9092",
         'group.id': "student_data",
-        'auto.offset.reset': 'earliest'}
-
-
+        'auto.offset.reset': 'earliest',
+        }
 consumer = Consumer(conf)
 
-def msg_process(msg):
-    data = {}
-    try:
-        last_insert = connection.execute("select max(stu_id) from student")
-        last_id = last_insert.all()[0][0]
-    except ConnectionError:
-        print("connection not established with database ")
-       
+def msg_process(msg):    
+    data = {}    
     data[msg.key().decode('utf-8')] = msg.value().decode('utf-8')
-    stu_detail_dict= {}
-    stu_phone_dict = {}
-    if last_id is None:
-        stu_detail_dict['stu_id'] = 1
-        stu_phone_dict['stu_id'] = 1
+    if data.values()==  "":
+        print("empty string")
+        return "empty string"
+    
     else:
-        print(stu_detail_dict)
-        last_id +=1
-        stu_detail_dict['stu_id'] = last_id
-        stu_phone_dict['stu_id'] = last_id
-    try:
-        for k,v in json.loads(data['sample']).items():
-            if type(v)==dict:
+        stu_detail_dict={}
+        stu_phone_dict={}
+        stu_id = ''.join([random.choice(string.ascii_letters
+                + string.digits) for num in range(10)]) 
+        stu_detail_dict['stu_id']=stu_id
+        stu_phone_dict['stu_id']=stu_id
+                 
+        for k,v in json.loads(data[msg.key().decode('utf-8')]).items():
+            if k=='firstName' and v=="":
+                return "First name cannot be empty" 
+            elif type(v)==dict:
                 for key, value in v.items():
                     stu_detail_dict[key]=value
             elif type(v)==list:
                 continue
-            else:
-                if k=='firstName' and v=="":
-                    return "First name cannot be empty"
-                stu_detail_dict[k]=v
-    except AttributeError:
-        print("empty file sent by producer")
-
-    details_columns = ', '.join(""+str(x)+"" for x in stu_detail_dict.keys())
-    details_values = ', '.join("'"+str(x)+"'" for x in stu_detail_dict.values())
-    insert_query = "INSERT INTO %s ( %s ) VALUES ( %s );" % ('student', details_columns, details_values)
-    connection.execute(insert_query)
-    
-
-    try:
-        for k,v in json.loads(data['sample']).items():
+            else:            
+                stu_detail_dict[k]=v   
+         
+        for k,v in json.loads(data[msg.key().decode('utf-8')]).items():
             if type(v)==list:
                 for i in range(len(v)):
                     for key, value in v[i].items():
                         stu_phone_dict[key]=value
-                    stu_contact_field = ', '.join(""+str(value)+"" for value in stu_phone_dict.keys())
-                    contact_values = ', '.join("'"+str(value)+"'" for value in stu_phone_dict.values())
-                    insert_statement= "INSERT INTO %s ( %s ) VALUES ( %s );" % ('student_contact', stu_contact_field, contact_values)
-                    connection.execute(insert_statement)
-    except AttributeError:
-        print("empty file sent by producer")
+   
+    student_header = ['stu_id','firstName','lastName','gender','age','streetAddress','city','state','postalCode']
+    student_contact_header = ['stu_id','type','number']
+    file_path_stu_contact = "/home/priyanshu/Desktop/Assignment2_python/student_contact.csv"
+    file_path_stu = "/home/priyanshu/Desktop/Assignment2_python/student.csv"
+    
+    if os.path.exists(f'{file_path_stu}'):
+        with open(f'{file_path_stu}','a') as csv_file:
+            csv_writer_ = csv.DictWriter(csv_file,student_header)        
+            csv_writer_.writerow({field: stu_detail_dict[field] for field in student_header})
+    else:
+        with open(f'{file_path_stu}','a') as csv_file:
+            csv_writer_ = csv.DictWriter(csv_file,student_header)
+            csv_writer_.writeheader()        
+            csv_writer_.writerow( stu_detail_dict )
+    
+            
+    if os.path.exists(f"{file_path_stu_contact}"):
+        with open(f"{file_path_stu_contact}",'a') as csv_file:
+            csv_writer_ = csv.DictWriter(csv_file,student_contact_header)        
+            csv_writer_.writerow(stu_phone_dict)
+    else:
+        with open(f"{file_path_stu_contact}",'a') as csv_file:
+            csv_writer_ = csv.DictWriter(csv_file,student_contact_header)
+            csv_writer_.writeheader()        
+            csv_writer_.writerow( stu_phone_dict )
+                        
 
 
 def basic_consume_loop(consumer, topics):
@@ -100,6 +76,8 @@ def basic_consume_loop(consumer, topics):
         while True:
             msg = consumer.poll(1)
             if msg is None: 
+                
+                print("hhehehehheh")          
                 break
 
             if msg.error():                
@@ -110,6 +88,8 @@ def basic_consume_loop(consumer, topics):
                 msg_process(msg)
     except KafkaException:
         print("Unknown topic or partition")
+    finally:
+        consumer.close()
    
 if __name__ == '__main__':
     basic_consume_loop(consumer, ['student_data'])
